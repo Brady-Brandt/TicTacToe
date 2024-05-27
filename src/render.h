c1b903f2a88e24733658b1be1887ecdc11b33305
Brady-Brandt <bradybrandt3@gmail.com>
2024-05-27 08:30:52 -0500

    Embedded textures into the program

--- src/render.h
+++ src/render.h
@@ -1,12 +1,8 @@
 #pragma once
 
 #include "game.h"
-#include <stdbool.h>
 #include <stdint.h>
 
-
-
-uint32_t load_texture(const char *path);
 
 //returns the shader id
 uint32_t render_init();
@@ -17,4 +13,3 @@ void draw_game(uint32_t shader, Symbol board[9]);
 
 void display_winner(uint32_t shader, int winner);
 
-void display_end_buttons(uint32_t shader);
